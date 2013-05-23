df6f228061e287fb8b859a812ec8d23c71e02985
Andrew Bunday <andrew.bunday@gmail.com>
2013-05-23 18:04:12 +0100

    first draft of a shows command for the shell

---
+++ src/etc/bash_aliases.d/shows-shell-alias.sh
@@ -0,0 +1,5 @@
+# sets the shell to find the 'shows' command
+
+alias shows='source /opt/baseblack/shows-shell/1/shows'
+alias job='source /opt/baseblack/shows-shell/1/shows'
+alias jobs='source /opt/baseblack/shows-shell/1/shows'
